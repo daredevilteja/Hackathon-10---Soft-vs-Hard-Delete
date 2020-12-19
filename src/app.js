5769a2d8f509ecc0e66bdced53a315bd5df2bf6d
Ravi Teja Natchireddi <daredevilteja@outlook.com>
2020-12-19 18:20:38 +0530

    Added CRUD operations

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const Student = require('./models/Student');
 
+const isNullOrUndefined = (val) => {
+    return val === null || val === undefined || val === "";
+
+}
 
 const app = express();
 
@@ -11,23 +15,46 @@ app.use(express.json());
 
 // Get all the students
 app.get('/students', async (req, res) => {
-    // write your codes here
+    res.send(await Student.find({"isDeleted": false}));
 })
 
 // Add student to database
-app.post('/students', async (req, res) =>{
+app.post('/students', async (req, res) => {
     // write your codes here
+    const newStudent = new Student(req.body);
+    await newStudent.save();
+    res.sendStatus(201);
 })
 
 // Get specific student
-app.get('/students/:id', async (req, res) =>{
+app.get('/students/:id', async (req, res) => {
     // write your codes here
+    const id = req.params.id;
+    const ans = await Student.findById(id);
+    res.send(ans);
 })
 
 // delete specific student
-app.delete('/students/:id', async (req, res) =>{
+app.delete('/students/:id', async (req, res) => {
     // write your codes here
-}) 
+    let type = req.query.type;
+    const id = req.params.id;
+
+    if(isNullOrUndefined(id)) {
+        res.sendStatus(404);
+    }
+    if(isNullOrUndefined(type)) {
+        type = "hard";
+    }
+
+    if(type === "soft") {
+        await Student.updateOne({"_id": id}, {"isDeleted": true});
+    } else {
+        await Student.deleteOne({"_id": id})
+    }
+    res.sendStatus(200);
+
+})
 
 
 module.exports = app;
